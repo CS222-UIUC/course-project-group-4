5e335be0593a95c38a07a3064a25f1e6c2522646
Allen Millington <milling1@illinois.edu>
2022-09-16 17:23:45 -0500

    instructed .gitignore to ignore .html files (due to pydocs)

--- gpa_disparity_py/network/data_sources.py
+++ gpa_disparity_py/network/data_sources.py
@@ -1,5 +1,5 @@
 """_summary_
-data sources for things like GPA data, class explorer base url, etc
+Data sources for things like GPA data, class explorer base url, etc
 """
 #gpa_url = "https://github.com/wadefagen/datasets/tree/master/gpa/raw"
 
