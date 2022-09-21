232fefdffd4f95119d5c3c59e9ec07efecfa9f57
Allen Millington <milling1@illinois.edu>
2022-09-21 15:31:46 -0500

    wrote a test case

--- gpa_disparity_py/network/test_network_utility.py
+++ gpa_disparity_py/network/test_network_utility.py
@@ -1,5 +1,5 @@
 """tests functiosn of network_requests.py """
-from network_utility import get_filename
+from network_utility import CACHE_PATH, get_filename, write_to_cache
 
 GPA = {
     'owner': 'wadefagen',
@@ -12,3 +12,16 @@ def test_get_filename():
     """ tests that get_filename is using sha256 and returns properly) """
     assert get_filename(
         "Hello World") == "a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e"
+
+
+def test_cache_file_creation():
+    """tests that the write to cache created a file"""
+    write_to_cache("Hello World", b"")
+    with open(CACHE_PATH + "a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e", "r") as binary_file:
+        assert(binary_file)
+
+
+# def test_write_to_cache():
+#     write_to_cache("Hello World", b"Hello World")
+#     with open(CACHE_PATH + "a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e", "wb") as binary_file:
+#         assert(binary_file.readline == b"Hello World")
