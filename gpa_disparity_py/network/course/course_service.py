7ab0dc2ff1e6bde0bb750ae0aa7f9ebe32573f78
Allen Millington <milling1@illinois.edu>
2022-09-29 20:13:16 -0500

    removed useless network_utility file

--- gpa_disparity_py/network/course/course_service.py
+++ gpa_disparity_py/network/course/course_service.py
@@ -1,3 +1,8 @@
-# wrapper class
-# check if in cache
-# else request
+from network.service import AbstractService
+
+
+class GpaService(AbstractService):
+    def validate_input(self, year, semester, subjectCode, courseNumber, crn):
+        if year < 2004:
+            raise ValueError("year must be > 2004 for CIS Data Explorer")
+    
