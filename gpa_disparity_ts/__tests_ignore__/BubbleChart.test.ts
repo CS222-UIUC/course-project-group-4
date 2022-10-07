44ec4b70c1f0d6663e59aa07fe8469a896bc721c
Allen Millington <milling1@illinois.edu>
2022-10-07 14:46:51 -0500

    added clarification to commandline usage / testing  of DataFetcher

--- gpa_disparity_ts/__tests_ignore__/BubbleChart.test.ts
+++ gpa_disparity_ts/__tests_ignore__/BubbleChart.test.ts
@@ -1,13 +1,13 @@
 import React from "react";
 import BubbleChart from "../../src/BubbleChart";
 
-// https://github.com/reactchartjs/react-chartjs-2/issues/155
+//github.com/reactchartjs/react-chartjs-2/issues/155
 
-// jest.mock("react-chartjs-2", () => ({
-//   Bubble: () => null,
-// }));
+https: jest.mock("react-chartjs-2", () => ({
+  Bubble: () => null,
+}));
 
-// it("Renders a BubbleChart", () => {
-//   const wrapper = mount(<BubbleChart />);
-//   expect(wrapper).toMatchSnapshot();
-// });
+it("Renders a BubbleChart", () => {
+  const wrapper = mount(<BubbleChart />);
+  expect(wrapper).toMatchSnapshot();
+});
--- gpa_disparity_ts/src/network/DataFetcher.ts
+++ gpa_disparity_ts/src/network/DataFetcher.ts
@@ -9,5 +9,6 @@ export default class DataFetcher {
 // example commandline usage(?):
 // npx ts-node -O '{"module":"commonjs"}' ./src/network/DataFetcher.ts
 
+// uncomment below if using above commands to test from commandline
 // const df = new DataFetcher();
 // df.fetchGpaInformation().then(console.log);
