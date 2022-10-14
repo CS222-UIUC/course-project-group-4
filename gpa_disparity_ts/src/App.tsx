726f3f20cc87f03c58f7d60565d3638df9369b19
Allen Millington <milling1@illinois.edu>
2022-10-14 15:03:33 -0500

    created a sample GpaInformationDisplay component.

--- gpa_disparity_ts/src/App.tsx
+++ gpa_disparity_ts/src/App.tsx
@@ -1,10 +1,35 @@
+import { useState } from "react";
 import "./App.css";
 import BubbleChart from "./BubbleChart";
+import GpaInformationDisplay, { GpaInformation } from "./GpaInformationDisplay";
+
+// Assume Existing Function
+// RequestGPAInformationFromPythonAPI(Year, CRN) -> Object containing Course information
+// mocked for this example
+const RequestGPAInformationFromPythonAPI = (crn: number, year: number) => {
+  const sample_class: GpaInformation = {
+    subject: "CS",
+    course_number: 125,
+    course_title: "Intro to Computer Science",
+    average_gpa: 3.5,
+  };
+  return sample_class;
+};
 
 function App() {
+  // Year & CRN are set by a dropdown component (implemented elsewhere)
+  // Year & CRN are read by GpaInformationDisplay
+  const [year, setYear] = useState(2020);
+  const [crn, setCRN] = useState(0);
+
   return (
     <div className="App">
-      <BubbleChart />
+      {/* <BubbleChart /> */}
+      <GpaInformationDisplay
+        year={year}
+        crn={crn}
+        request_gpa_info={RequestGPAInformationFromPythonAPI}
+      />
     </div>
   );
 }
---
+++ gpa_disparity_ts/src/GpaInformationDisplay.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+
+interface GpaInformationDisplayProps {
+  crn: number;
+  year: number;
+  request_gpa_info: any; // FIXME
+}
+
+/* This interface should be in its own file (GpaInformation.tsx) as
+it will also be used by database / data management
+Also good practice to keep data separate from code
+It's only in this file for easier viewing as sample code.
+*/
+export interface GpaInformation {
+  subject: string;
+  course_number: number;
+  course_title: string;
+  average_gpa: number;
+}
+
+const GpaInformationDisplay = (props: GpaInformationDisplayProps) => {
+  const [gpa_info, setGpaInfo] = useState({} as GpaInformation);
+
+  useEffect(() => {
+    setGpaInfo(props.request_gpa_info(props.year, props.crn));
+  }, [props.crn, props.year, props]); // will update when the crn or year changes
+
+  console.log(gpa_info);
+
+  return (
+    <div className="gpa-information-display">
+      <p>
+        <b> subject: </b> {gpa_info.subject}
+        {/* linebreak using <br /> tag*/}
+        <br />
+        <b> course: </b> {gpa_info.course_number}
+        <br />
+        <b> course_title: </b> {gpa_info.course_title}
+        <br />
+        <b> average_gpa: </b> {gpa_info.average_gpa}
+        <br />
+      </p>
+    </div>
+  );
+};
+
+export default GpaInformationDisplay;
