8dc3abb9506bc270ac2fb15a0e8890b6c3f5bcc4
Jason Kandula <kandula7@illinois.edu>
2022-10-14 22:08:54 -0500

    implemented generic dropdown component

--- gpa_disparity_ts/src/App.tsx
+++ gpa_disparity_ts/src/App.tsx
@@ -2,51 +2,21 @@ import React from 'react';
 //for useState hook
 import { useState } from 'react';
 import logo from './logo.svg';
-
 import './App.css';
+import DropDown from './dropdown';
 
-/*
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
 
-//should be for subjects
-export default function BasicSelect() {
-  const [subject, setSubject] = React.useState('');
+function retrieveSubjectsFromDB() {
+  return ["CS", "ECE", "ME", "MEB"];
+}
 
-  const handleChange = (event: SelectChangeEvent) => {
-    setSubject(event.target.value as string);
-  };
-
-      <Box sx={{ minWidth: 120 }}>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Age</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={age}
-          label="Age"
-          onChange={handleChange}
-        >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>
-    </Box>
-
-*/
-
-//RequestSubjectsFromPythonAPI -> list(subjects for DropDown Selector)
-//RequestGPAInformationFromPythonAPI-> list(GPA info for a graph component)
 
 function App() {
+  const [subject, setSubject] = React.useState<string | number>(0);
   return (
-
     <div className="App">
       <header className="App-header">
+        <DropDown retrieveMenuItems={retrieveSubjectsFromDB} value={subject} setValue={setSubject} label="Subject"/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
