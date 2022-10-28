ebad59211094c9b6a89e14af77eddb46a6acb863
Joe Lian <jlian5@illinois.edu>
2022-10-28 14:59:21 -0500

    Added getAll major functionality and real tests

--- gpa_disparity_py/database/courses_test.py
+++ gpa_disparity_py/database/courses_test.py
@@ -1,5 +1,20 @@
 from db import initialize_db
-
+from domain.courses import CoursesDomain
+from domain.courses import CoursesModel
+from repository.courses import CoursesRepository
+import pandas as pd
+df = pd.read_csv("gpa_disparity_py/test_files/fa2014.csv", dtype=str)
 resource = initialize_db()
+course_domain = CoursesDomain(CoursesRepository(resource))
+NUM_ITEMS = 6
+test_crns = set()
+for i in range(NUM_ITEMS):
+    d = dict(df.iloc[i])
+    d["semester"] = "example"
+    course_domain.create_course_json(d)
+    test_crns.add(d["CRN"])
+    
+assert "AAS" in course_domain.get_all_majors()
 
-assert resource.Table("Courses").table_status == "ACTIVE"
+for crn in test_crns:
+    course_domain.delete_course(crn, "example")
