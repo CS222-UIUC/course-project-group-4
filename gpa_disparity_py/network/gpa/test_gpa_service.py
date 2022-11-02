730f98c13246358e74163772a1a6c15bfd76e532
Allen Millington <milling1@illinois.edu>
2022-11-01 21:38:03 -0500

    added test_gpa_service.py

---
+++ gpa_disparity_py/network/gpa/test_gpa_service.py
@@ -0,0 +1,10 @@
+import unittest
+
+from network.gpa.gpa_fetcher import GpaFetcher
+from network.gpa.gpa_service import GpaService
+
+
+class query_gpa_information(unittest.TestCase):
+    def test_query_gpa_information(self):
+        gpa_service = GpaService()
+        self.assertFalse(gpa_service.query_gpa_information("CS"))
