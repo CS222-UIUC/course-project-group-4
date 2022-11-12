7d9d2ca660706b83e24aa778eec8210ffc041c4d
Allen Millington <milling1@illinois.edu>
2022-11-11 22:31:41 -0600

    fixed some errors

--- gpa_disparity_ts/src/AggregateGPA.tsx
+++ gpa_disparity_ts/src/AggregateGPA.tsx
@@ -1,7 +1,5 @@
-import { useEffect, useState } from "react";
 import ClassInfo from "./ClassInfo";
 import * as lo from "lodash";
-import { sample_classes } from "./sample_class_list";
 
 interface ClassList {
   classes: ClassInfo[];
@@ -14,40 +12,65 @@ export interface AggregateInfo {
   average_class_size: number;
 }
 // calculates data for each specific course
-function processCalculations(props: ClassInfo[]) {
-    var total_count = 0;
-    var classes_count = props.length;
-    var gpa_total = 0;
+export function processCalculations(props: ClassInfo[]) {
+  var total_count = 0;
+  var classes_count = props.length;
+  var gpa_total = 0;
 
-    props.forEach((element) => {
-        // number of students count basically
-        var add1 = Number(element.a_plus) + Number(element.a) + Number(element.a_minus) + Number(element.b_plus)
-        + Number(element.b) + Number(element.b_minus) + Number(element.c_plus) + Number(element.c) + Number(element.c_minus)
-        + Number(element.d_plus) + Number(element.d) + Number(element.d_minus) + Number(element.f);
-        total_count = total_count + add1;
+  props.forEach((element) => {
+    // number of students count basically
+    var add1 =
+      Number(element.a_plus) +
+      Number(element.a) +
+      Number(element.a_minus) +
+      Number(element.b_plus) +
+      Number(element.b) +
+      Number(element.b_minus) +
+      Number(element.c_plus) +
+      Number(element.c) +
+      Number(element.c_minus) +
+      Number(element.d_plus) +
+      Number(element.d) +
+      Number(element.d_minus) +
+      Number(element.f);
+    total_count = total_count + add1;
 
-        // gpa calculation
-        var add2 = 4*Number(element.a_plus) + 4*Number(element.a) + 3.67*Number(element.a_minus) + 3.33*Number(element.b_plus)
-        + 3*Number(element.b) + 2.67*Number(element.b_minus) + 2.33*Number(element.c_plus) + 2*Number(element.c) + 1.67*Number(element.c_minus)
-        + 1.67*Number(element.d_plus) + Number(element.d) + .67*Number(element.d_minus) + 0*Number(element.f);
-        gpa_total = gpa_total + add2;
-    })
-    var avg_gpa = gpa_total/total_count;
-    var avg_size = total_count/classes_count;
-    const info: AggregateInfo = {subject: props[0].subject, class_number: props[0].number, average_gpa: avg_gpa, average_class_size: avg_size};
-    return info;
+    // gpa calculation
+    var add2 =
+      4 * Number(element.a_plus) +
+      4 * Number(element.a) +
+      3.67 * Number(element.a_minus) +
+      3.33 * Number(element.b_plus) +
+      3 * Number(element.b) +
+      2.67 * Number(element.b_minus) +
+      2.33 * Number(element.c_plus) +
+      2 * Number(element.c) +
+      1.67 * Number(element.c_minus) +
+      1.67 * Number(element.d_plus) +
+      Number(element.d) +
+      0.67 * Number(element.d_minus) +
+      0 * Number(element.f);
+    gpa_total = gpa_total + add2;
+  });
+  var avg_gpa = gpa_total / total_count;
+  var avg_size = total_count / classes_count;
+  const info: AggregateInfo = {
+    subject: props[0].subject,
+    class_number: props[0].number,
+    average_gpa: avg_gpa,
+    average_class_size: avg_size,
+  };
+  return info;
 }
 
-
-const AggregateGPA = (props: ClassList) => {
-  const { classes } = props;
-  const classes_info = lo.groupBy(classes, "number");
+const AggregateGPA = (props: ClassInfo[]) => {
+  const classes_info = lo.groupBy(props, "number");
   const classes_data: AggregateInfo[] = [];
-    for (const number in classes_info) {
-       classes_data.push(processCalculations(classes_info[number]));
-    }
-    // having issues looping thru array and printing out the information in a paragraph or smth
-    return (classes_data);
-};  
+  for (const number in classes_info) {
+    classes_data.push(processCalculations(classes_info[number]));
+  }
+  // having issues looping thru array and printing out the information in a paragraph or smth
+  return classes_data;
+};
 
 export default AggregateGPA;
