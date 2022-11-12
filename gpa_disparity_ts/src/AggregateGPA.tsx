6dc1b4f9bcb7c8f47520db11d05f8add4f94f71a
Allen Millington <milling1@illinois.edu>
2022-11-11 18:33:54 -0600

    added lo-dash, sample class_lists from db and after lodash processing

--- gpa_disparity_ts/src/AggregateGPA.tsx
+++ gpa_disparity_ts/src/AggregateGPA.tsx
@@ -1,31 +1,32 @@
 import { useEffect, useState } from "react";
 import ClassInfo from "./ClassInfo";
+import * as lo from "lodash";
+import { sample_classes } from "./sample_class_list";
 
 interface ClassList {
-    classes: ClassInfo[]
+  classes: ClassInfo[];
 }
 
 export interface AggregateInfo {
-    subject: string;
-    class_number: string;
-    average_gpa: number;
-  }
+  subject: string;
+  class_number: string;
+  average_gpa: number;
+  average_class_size: number;
+}
 
-const AggregateGPA = (props:ClassList) => {
-    // var total_grades1 = Number(props.FallECE333366168573221121.a_plus_val) + Number(props.FallECE333366168573221121.a_val)
-    // + Number(props.FallECE333366168573221121.a_minus_val) + Number(props.FallECE333366168573221121.b_plus_val) + Number(props.FallECE333366168573221121.b_val) + 
-    // Number(props.FallECE333366168573221121.b_minus_val) + Number(props.FallECE333366168573221121.c_plus_val) + Number(props.FallECE333366168573221121.c_val)
-    // + Number(props.FallECE333366168573221121.c_minus_val) + ;
-    var classes_info = props.classes.group(({ class_number }) => class_number);
+const AggregateGPA = (props: ClassList) => {
+  // var total_grades1 = Number(props.FallECE333366168573221121.a_plus_val) + Number(props.FallECE333366168573221121.a_val)
+  // + Number(props.FallECE333366168573221121.a_minus_val) + Number(props.FallECE333366168573221121.b_plus_val) + Number(props.FallECE333366168573221121.b_val) +
+  // Number(props.FallECE333366168573221121.b_minus_val) + Number(props.FallECE333366168573221121.c_plus_val) + Number(props.FallECE333366168573221121.c_val)
+  // + Number(props.FallECE333366168573221121.c_minus_val) + ;
+  const { classes } = props;
+  const classes_info = lo.groupBy(classes, "number");
 
-    
-    return (
-      <div className="aggregate-gpa-display">
-        <p>
-         
-        </p>
-      </div>
-    );
-  };
-  
+  return (
+    <div className="aggregate-gpa-display">
+      <p></p>
+    </div>
+  );
+};
+
 export default AggregateGPA;
