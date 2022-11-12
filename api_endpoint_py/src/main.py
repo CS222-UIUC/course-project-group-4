2288e52732a96fc0cb98ff08ed1516b4f65e631c
milling1 <111776439+MillingtonAllen1@users.noreply.github.com>
2022-11-11 20:16:47 -0600

    Changed python api to be compatible with Cross-origin resource sharing (#42)

--- api_endpoint_py/src/main.py
+++ api_endpoint_py/src/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
-
 from network.api_service import ApiService
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# https://stackoverflow.com/questions/65635346/how-can-i-enable-cors-in-fastapi
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    # allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 api_service = ApiService()
 
 
