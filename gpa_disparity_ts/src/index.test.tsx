43784d07bd100996d4fea7c86c8f843f508bf2b1
Jason Kandula <kandula7@illinois.edu>
2022-11-19 21:07:20 -0500

    created test file

--- gpa_disparity_ts/src/index.test.tsx
+++ gpa_disparity_ts/src/index.test.tsx
@@ -2,3 +2,4 @@ import React from "react"
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import { BrowserRouter, MemoryRouter } from "react-router-dom"
+
