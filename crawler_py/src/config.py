620024027332d705ccc755dfc794623e0270bd05
Allen Millington <milling1@illinois.edu>
2022-11-29 21:03:35 -0600

    Added start and end year to crawler

--- crawler_py/src/config.py
+++ crawler_py/src/config.py
@@ -1,5 +1,6 @@
 from pydantic import BaseSettings
 import dotenv
+import datetime
 
 
 class Config(BaseSettings):
@@ -7,7 +8,8 @@ class Config(BaseSettings):
     write_endpoint_key = ""
     github_access_token: str = ""
     subject_to_load = ""
-    year_to_load = ""
+    start_year = ""
+    end_year = str(datetime.date.today().year)
     log_level: str = "WARN"
 
 
