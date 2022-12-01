3af6175ca86242eb1f4897be24ce058cfb912953
Allen Millington <milling1@illinois.edu>
2022-11-30 23:11:24 -0600

    removed unneeded github key from endpoint .env.example and config.py

--- api_py/src/config.py
+++ api_py/src/config.py
@@ -6,7 +6,6 @@ class Config(BaseSettings):
     aws_access_key_id: str = ""
     aws_secret_key: str = ""
     log_level: str = "WARN"
-    github_access_token: str = ""
     write_endpoint_key = ""
 
 
