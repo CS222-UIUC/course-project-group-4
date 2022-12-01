94b363ad5bfaec1d0fbe7f6dfa7b14e40ae53fb3
Allen Millington <milling1@illinois.edu>
2022-12-01 00:32:36 -0600

    fixed off by one error on crawler

--- crawler_py/src/network/course_info/course_info_fetcher.py
+++ crawler_py/src/network/course_info/course_info_fetcher.py
@@ -88,7 +88,7 @@ class CourseInfoFetcher:
             _type_: _description_
         """
         course_info = []
-        for year in list(range(int(self._MAX_YEAR), int(self._MIN_YEAR), -1)):
+        for year in list(range(int(self._MAX_YEAR), int(self._MIN_YEAR) - 1, -1)):
             for semester in [Semester.FALL, Semester.SPRING]:
                 course_info_append = self.get_course_info_helper(year, semester)
                 logging.info(f"retrieved (${year}, ${semester}): ${course_info_append}")
